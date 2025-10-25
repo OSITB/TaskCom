3f7a365e996b60b07df166d4e85855eba3ff31d3
IliyaBykov <osipv6@icloud.com>
2025-10-25 21:22:53 +0300

    Initial Commit

---
+++ TaskCom/TaskComApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskComApp.swift
+//  TaskCom
+//
+//  Created by Илья Быков on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
