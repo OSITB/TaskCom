e879755c46a8e82fb4b130d38e1a051ea0daa798
IliyaBykov <osipv6@icloud.com>
2025-11-05 20:12:36 +0300

    Добавлены CRUD-методы на создание, обновление и редактирование записей.

--- TaskCom/ViewModels/TaskListViewModel.swift
+++ TaskCom/ViewModels/TaskListViewModel.swift
@@ -21,4 +21,41 @@ class TaskListViewModel: ObservableObject {
     func loadTask() {
         tasks = coreDataManager.fetchTasks()
     }
+    
+    // СОЗДАНИЕ
+    func addTask(title: String, description: String?, priority: Priority, dueDate: Date?) {
+        let newTask = Task(
+            id: UUID(),
+            title: title,
+            taskDescription: description,
+            priority: priority,
+            isCompleted: false,
+            createdAt: Date(),
+            dueDate: dueDate,
+            completedAt: nil)
+        
+        coreDataManager.create(newTask)
+        tasks.append(newTask)
+    }
+    
+    // ОБНОВЛЕНИЕ (отметить выполненной)
+    func toggleCompletion(for task: Task) {
+        guard let index = tasks.firstIndex(where: { $0.id == task.id}) else { return }
+        
+        var updatedTask = tasks[index]
+        updatedTask.isCompleted.toggle()
+        updatedTask.completedAt = updatedTask.isCompleted ? Date() : nil
+        
+        coreDataManager.update(updatedTask)
+        tasks[index] = updatedTask
+    }
+    
+    // УДАЛЕНИЕ
+    func deleteTask(_ task: Task) {
+        coreDataManager.delete(task)
+        if let index = tasks.firstIndex(where: { $0.id == task.id}) {
+            tasks.remove(at: index)
+        }
+    }
+    
 }
